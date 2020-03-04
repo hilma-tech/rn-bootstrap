6d3f3ef0312536427a8abaff649b4235b04c07d6
nogalevy <nogalevy2@gmail.com>
2020-03-04 15:32:00 +0200

    create 'Noga' folder and add new route for register that create new user in server and save its data in asyncStorage

---
+++ Noga/HomeScreen.js
@@ -0,0 +1,172 @@
+import React, { Component } from 'react';
+import { Alert, Button, TextInput, View, StyleSheet } from 'react-native';
+import { AsyncStorage } from 'react-native';
+
+import HooksFactory from "../Maayan/HooksFactory"
+import Auth from "./../src/modules/auth/Auth"
+
+// function getCookieByKey(name) { // mv to tools/cookeis  getcookieByKey
+//   var value = "; " + document.cookie;
+//   var parts = value.split("; " + name + "=");
+//   if (parts.length === 2) return parts.pop().split(";").shift();
+// }
+
+function verboseAsyncStorage() {
+  AsyncStorage.getAllKeys((err, keys) => {
+    AsyncStorage.multiGet(keys, (error, stores) => {
+      stores.map((result, i, store) => {
+        console.log({ [store[i][0]]: store[i][1] });
+        return true;
+      });
+    });
+  });
+}
+
+/*
+function superFetch(url, payload) {
+
+    let fPromise = payload == null ? fetch(url) : fetch(url, payload);
+
+    return new Promise((resolve, reject) => {
+      fPromise
+        .then(this.parseJSON)// this trys to parse- get origin error when you have one.
+        .then((response) => {
+          if (response.ok) {
+            return resolve([response.json, null]);
+          }
+          // extract the error from the server's json
+          return resolve([null, response.json]);
+        })
+        .catch((error) => resolve([null, { error: { message: "No response, check your network connectivity", statusCode: 500, name: "ERROR" } }]));
+    });
+}
+*/
+
+
+export default class HomeScreen extends Component {
+  constructor(props) {
+    super(props);
+
+    window.HooksFactory = new HooksFactory();
+    window.hooksRepository = window.HooksFactory.getRepository();
+    window.hooksRepository.addHook("auth", "AFTER_LOGIN", () => { console.log("hii from home screen") });
+    this.state = {
+      username: 'admin@carmel6000.amitnet.org',
+      password: 'E2PSzAmJ-5-ldKnl',
+    };
+  }
+
+  async showUsersList() {
+
+    let res = null;
+    try {
+      let at = await AsyncStorage.getItem('access_token');
+
+      at = "?access_token=" + at;
+
+      res = await fetch('https://pumba.carmel6000.com/api/CustomUsers' + at, {
+        //credentials: "same-origin" //or "include" both doesn't work
+      }).then(res => res.json());
+
+      console.log("USERS list res", res);
+
+    } catch (err) {
+      console.log("err", err);
+    }
+
+  }
+
+  async onLogin() {
+    const { username, password } = this.state;
+
+    //Alert.alert('Credentials', `${username} + ${password}`);
+    console.log('Credentials', `${username} + ${password}`);
+
+    let res = null;
+    try {
+      res = await Auth.login(username, password, res => res.json());
+      // res = await fetch('https://pumba.carmel6000.com/api/CustomUsers/elogin/', {
+      //   method: 'POST', 
+      //   credentials: 'same-origin',
+      //   headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+      //   body: JSON.stringify({ email: username, password: password })
+      // }).then(res=>res.json());
+
+
+
+      console.log("login res?", res);
+
+      console.log("document.cookie?", document.cookie);
+
+      await AsyncStorage.setItem('klo', res.klo);
+      await AsyncStorage.setItem('kl', res.kl);
+      await AsyncStorage.setItem('kloo', res.kloo);
+      await AsyncStorage.setItem('klk', res.klk);
+      await AsyncStorage.setItem('access_token', res.id);
+
+      verboseAsyncStorage();
+
+    } catch (err) {
+      console.log("login err?", err);
+    }
+
+
+
+
+  }
+
+  render() {
+    const { navigate } = this.props.navigation;
+
+    return (
+      <View style={styles.container}>
+        <TextInput
+          value={this.state.username}
+          onChangeText={(username) => this.setState({ username })}
+          placeholder={'Username'}
+          style={styles.input}
+        />
+        <TextInput
+          value={this.state.password}
+          onChangeText={(password) => this.setState({ password })}
+          placeholder={'Password'}
+          secureTextEntry={true}
+          style={styles.input}
+        />
+
+        <Button
+          title={'Login'}
+          style={styles.input}
+          onPress={this.onLogin.bind(this)}
+        />
+
+        <Button
+          title={'Show users list'}
+          style={styles.input}
+          onPress={this.showUsersList.bind(this)} />
+
+        <Button
+          title={'register'}
+          style={styles.input}
+          onPress={() => navigate('Reg')} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+  },
+  input: {
+    width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+    marginBottom: 10,
+  },
+});
---
+++ Noga/RegistrationForm.js
@@ -0,0 +1,161 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  TouchableHighlight,
+  Image,
+  Alert
+} from 'react-native';
+
+import { AsyncStorage } from 'react-native';
+import HooksFactory from '../Maayan/HooksFactory';
+import Auth from '../src/modules/auth/Auth'
+
+export default class RegistrationForm extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      fullName: 'spongebob',
+      email: 'imspongebob@gmail.com',
+      password: 'spongeBob1234',
+    }
+    window.HooksFactory = new HooksFactory();
+    window.hooksRepository = window.HooksFactory.getRepository();
+    window.hooksRepository.addHook("auth", "AFTER_REGISTER", this.setUserDataInASyncStorage);
+
+    // this.hooksRepository = hooksFactory.getRepository();
+    // hooksRepository.addHook("auth", "AFTER_REGISTER", this.setUserDataInASyncStorage);
+  }
+
+  onClickListener = (viewId) => {
+    console.log("Alert", "Button pressed " + viewId);
+    this.onLogin()
+  }
+
+  onLogin = async () => {
+    const { fullName, password, email } = this.state;
+
+    //Alert.alert('Credentials', `${username} + ${password}`);
+    console.log('Credentials', `${fullName} + ${password}`);
+    let userData = {
+      realm: fullName,
+      username: fullName,
+      email: email,
+      emailVerified: true,
+      password: password
+    }
+
+    // let res = null;
+    const [res, err] = await Auth.superAuthFetch('https://pumba.carmel6000.com/api/CustomUsers/', {
+      method: 'POST',
+      credentials: 'same-origin',
+      headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+      body: JSON.stringify(userData)
+    })
+    if (res) {
+      console.log("login res?", res);
+      console.log("document.cookie?", document.cookie);
+      // verboseAsyncStorage();
+      window.hooksRepository.applyHook("auth", "AFTER_REGISTER", res);
+    }
+    if (err) console.log("login err?", err);
+  }
+
+  setUserDataInASyncStorage = async (res) => {
+    await AsyncStorage.setItem('user_res', JSON.stringify(res));
+
+  }
+
+  render() {
+    console.log("egeger")
+    return (
+      <View style={styles.container}>
+        <View style={styles.inputContainer}>
+          {/* <Image style={styles.inputIcon} source={{uri: 'https://png.icons8.com/male-user/ultraviolet/50/3498db'}}/> */}
+          <TextInput style={styles.inputs}
+            placeholder="Full name"
+            keyboardType="email-address"
+            underlineColorAndroid='transparent'
+            value={this.state.fullName}
+            onChangeText={(fullName) => this.setState({ fullName })} />
+        </View>
+
+        <View style={styles.inputContainer}>
+          {/* <Image style={styles.inputIcon} source={{uri: 'https://png.icons8.com/message/ultraviolet/50/3498db'}}/> */}
+          <TextInput style={styles.inputs}
+            placeholder="Email"
+            keyboardType="email-address"
+            underlineColorAndroid='transparent'
+            value={this.state.email}
+            onChangeText={(email) => this.setState({ email })} />
+        </View>
+
+        <View style={styles.inputContainer}>
+          {/* <Image style={styles.inputIcon} source={{uri: 'https://png.icons8.com/key-2/ultraviolet/50/3498db'}}/> */}
+          <TextInput style={styles.inputs}
+            placeholder="Password"
+            secureTextEntry={true}
+            underlineColorAndroid='transparent'
+            value={this.state.password}
+            onChangeText={(password) => this.setState({ password })} />
+        </View>
+
+        <TouchableHighlight style={[styles.buttonContainer, styles.signupButton]} onPress={() => this.onClickListener('sign_up')}>
+          <Text style={styles.signUpText}>Sign up</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    borderBottomColor: '#F5FCFF',
+    backgroundColor: '#FFFFFF',
+    borderRadius: 30,
+    borderBottomWidth: 1,
+    width: 250,
+    height: 45,
+    marginBottom: 20,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  inputs: {
+    height: 45,
+    marginLeft: 16,
+    borderBottomColor: '#717D93',
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#939BA9',
+  },
+  inputIcon: {
+    width: 30,
+    height: 30,
+    marginLeft: 15,
+    justifyContent: 'center'
+  },
+  buttonContainer: {
+    height: 45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+    width: 250,
+    borderRadius: 30,
+  },
+  signupButton: {
+    backgroundColor: "#3C444F",
+  },
+  signUpText: {
+    color: 'white',
+  }
+});
+
