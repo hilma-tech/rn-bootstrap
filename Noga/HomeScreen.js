3a24fcb7eaf26dfb0e4604ba6d103882d0f5ae7b
maayankoren <nghieuri@gmail.com>
2020-03-16 15:38:39 +0200

    changes

--- Noga/HomeScreen.js
+++ Noga/HomeScreen.js
@@ -1,14 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import { Alert, Button, Text, View, StyleSheet } from 'react-native';
+import { AsyncStorage } from 'react-native';
 
 function HomeScreen(props) {
     // Declare a new state variable, which we'll call "count"
     const [count, setCount] = useState(0);
+    const [accessToken, setAccessToken] = useState("");
 
+    useEffect(() => {
+        let accessToken = await AsyncStorage.getItem("access_token");
+        setAccessToken(accessToken)
+         
+    }, []);
     return (
         //    <Text>HomeScreen</Text>
         <View>
-            <Text>Hello! please
+            <Text>{accessToken?`Hello! ${userNam} please`}
             <Text style={{ color: 'blue' }}
                     onPress={() => props.navigation.navigate('Login')} >
                      Login
