c859b966bb2d3fd4ee345ae3f7f58eaea1df2b4d
nogalevy <nogalevy2@gmail.com>
2020-03-16 15:36:37 +0200

    add floating label input component and style the login screen and add validate fields to login (from tools moudle)

--- Noga/LoginScreen.js
+++ Noga/LoginScreen.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
-import { Alert, Button, TextInput, View, StyleSheet } from 'react-native';
+import { Alert, Button, TextInput, Text, View, StyleSheet, TouchableHighlight } from 'react-native';
 import { AsyncStorage } from 'react-native';
 import consts from "../src/modules/tools/client/hooks/consts"
 import hooksFactory from "../src/modules/tools/client/hooks/HooksFactory"
 import Auth from "../src/modules/auth/Auth"
+import ValidateFields from '../src/modules/tools/ValidateFields'
+import FloatingLabelInput from '../components/FloatingLabelInput.js'
 
 export default class HomeScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: 'admin@carmel6000.amitnet.org',
-      password: 'E2PSzAmJ-5-ldKnl',
+      email: "", //'admin@carmel6000.amitnet.org',
+      password: "",//'E2PSzAmJ-5-ldKnl',
+      emailOutput: '',
+      passwordOutput: ''
     };
     this.hooksRepository = hooksFactory.getRepository();
     this.hooksRepository.addHook(consts.AUTH, consts.HOOK__AFTER_LOGIN, (x) => { console.log("hiiii from home screen", x) });
@@ -32,11 +36,26 @@ export default class HomeScreen extends Component {
   // }
 
   async onLogin() {
-    const { username, password } = this.state;
-    console.log('Credentials', `${username} + ${password}`);
+    const { email, password } = this.state;
+    console.log('Credentials', `${email} + ${password}`);
+
+    let emailOutput = ValidateFields.validateEmailInput(email, true)
+    let passwordOutput = ValidateFields.validatePasswordInput(password, true);
+
+    console.log("emailOutput", emailOutput, "passwordOutput", passwordOutput)
+
+    if (emailOutput !== '') {
+      this.setState({ emailOutput });
+      return;
+    }
+    if (passwordOutput !== '') {
+      this.setState({ passwordOutput });
+      return;
+    }
+
     let res = null;
     try {
-      res = await Auth.login(username, password, res => res.json());
+      res = await Auth.login(email, password, res => res.json());
       // console.log("login res?", res);
       // console.log("document.cookie?", document.cookie);
       // await AsyncStorage.setItem('klo', res.klo);
@@ -50,9 +69,9 @@ export default class HomeScreen extends Component {
     }
   }
 
-  handleChange = (nativeEvent, value) => {
-    console.log("valuee",value,  nativeEvent)
-    // this.setState({ []: value })
+  handleChange = (name, value) => {
+    console.log("valuee", name)
+    this.setState({ [name]: value })
   }
 
   render() {
@@ -60,24 +79,52 @@ export default class HomeScreen extends Component {
 
     return (
       <View style={styles.container}>
-        <TextInput
-        name = "nameee"
-          value={this.state.username}
-          // onChangeText={(username) => this.setState({ username })}
-          onChange={(nativeEvent, value) => this.handleChange(nativeEvent, value)}
-          placeholder={'Username'}
+        {/* <TextInput
+          value={this.state.email}
+          onChangeText={(value) => this.handleChange("email", value)}
+          placeholder={'Email'}
           style={styles.input}
-        />
-        <TextInput
-          value={this.state.password}
-          // onChangeText={(password) => this.setState({ password })}
-          onChange={(nativeEvent) => this.handleChange(nativeEvent)}
-          placeholder={'Password'}
-          secureTextEntry={true}
+        /> */}
+
+        <FloatingLabelInput
+          label="Email"
+          keyName="email"
+          value={this.state.email}
+          onChangeText={(keyName, value) => this.handleChange(keyName, value)}
           style={styles.input}
         />
 
-        <Button
+        {this.state.emailOutput ?
+          <Text>{this.state.emailOutput}</Text>
+          : null}
+        {/* <TextInput
+          value={this.state.password}
+          onChangeText={(value) => this.handleChange("password", value)}
+          placeholder={'Password'}
+          style={styles.input}
+          secureTextEntry={true}
+        /> */}
+
+        <FloatingLabelInput
+          label="Password"
+          keyName="password"
+          value={this.state.password}
+          onChangeText={(keyName, value) => this.handleChange(keyName, value)}
+          style={styles.input}
+          secureTextEntry={true}
+        />
+
+        {this.state.passwordOutput ?
+          <Text>{this.state.passwordOutput}</Text>
+          : null}
+
+
+        <TouchableHighlight style={[styles.buttonContainer, styles.signupButton]} onPress={this.onLogin.bind(this)}>
+          <Text style={styles.signUpText}>Login</Text>
+        </TouchableHighlight>
+
+        <Text style={{ color: '#1976d2', marginTop: 10 }} onPress={() => navigate('Registeration')}>Not registered?</Text>
+        {/* <Button
           title={'Login'}
           style={styles.input}
           onPress={this.onLogin.bind(this)}
@@ -86,7 +133,7 @@ export default class HomeScreen extends Component {
         <Button
           title={'register'}
           style={styles.input}
-          onPress={() => navigate('Reg')} />
+          onPress={() => navigate('Reg')} /> */}
       </View>
     );
   }
@@ -109,11 +156,25 @@ const styles = StyleSheet.create({
     backgroundColor: '#ecf0f1',
   },
   input: {
-    width: 200,
-    height: 44,
-    padding: 10,
-    borderWidth: 1,
-    borderColor: 'black',
-    marginBottom: 10,
+    // width: 200,
+    // height: 44,
+    // padding: 10,
+    // borderWidth: 1,
+    // borderColor: 'black',
+    // marginBottom: 10,
   },
+  buttonContainer: {
+    height: 30,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+    width: 300,
+  },
+  signupButton: {
+    backgroundColor: "#1976d2",
+  },
+  signUpText: {
+    color: 'white',
+  }
 });
